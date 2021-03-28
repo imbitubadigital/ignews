018fa889a6dece3cbd9f668ff000f68c83c2e72c
Antonio Fernandes <imbitubadigital@gmail.com>
2021-03-28 20:48:20 -0300

    adicionando stripe

--- src/components/SigninButton/index.tsx
+++ src/components/SigninButton/index.tsx
@@ -1,20 +1,24 @@
 
 import {FaGithub} from 'react-icons/fa'
 import {FiX} from 'react-icons/fi'
+import {signIn, useSession, signOut} from 'next-auth/client'
 import styles from './styles.module.scss'
 
 export function SignInButton(){
-  const isUserLoggedIn = true;
-  return isUserLoggedIn ? (
-    <button className={styles.signInButton} type="button">
+  const [session] = useSession()
+
+  console.log('session', session);
+
+  return session ? (
+    <button className={styles.signInButton} type="button" onClick={() => signOut()}>
       <FaGithub color="#04d361" />
-      Signin with Github
-      </button>
-  ) : (
-    <button className={styles.signInButton} type="button">
-      <FaGithub color="#eba417" />
-      Antonio Fernandes
+      {session.user.name}
       <FiX color="#737380" className={styles.closeIcon} />
-      </button>
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button" onClick={() => signIn('github')}>
+      <FaGithub color="#eba417" />
+      Signin with Github
+    </button>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import styles from '../styles/pages/home.module.scss'
 import Head from 'next/head'
 import {SubscribeButton} from '../components/SubscribeButton'
-export default function Home() {
+import { GetServerSideProps, GetStaticProps } from 'next'
+import { stripe } from '../services/stripe'
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
     <Head>
@@ -12,14 +21,60 @@ export default function Home() {
         <span>👏 Hey, welcome</span>
         <h1>News about the <span>React</span> world.</h1>
         <p>Get access to all the publications <br/>
-          <span>for $9.90 month</span>
+          <span>for {product.amount} month</span>
         </p>
-        <SubscribeButton />
+        <SubscribeButton priceId={product.priceId} />
       </section>
       <img src="/images/avatar.svg" alt="Girl coding" />
     </main>
-
-
     </>
   )
 }
+
+
+/* getServerSideProps: cria página estática e so recria conforme
+  o tem informado em segundos no parâmetro <revalidate>
+*/
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IZyU2DbumPO1jeQgC5mCp3A', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-Us', {
+      style: 'currency',
+      currency: "USD"
+    }).format(price.unit_amount / 100)
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 //24 horas
+  }
+}
+
+/* getServerSideProps: chama toda vez que carrega a pagina */
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve('price_1IZyU2DbumPO1jeQgC5mCp3A', {
+//     expand: ['product']
+//   })
+
+//   console.log('price', price);
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat('en-Us', {
+//       style: 'currency',
+//       currency: "USD"
+//     }).format(price.unit_amount / 100)
+//   }
+
+//   return {
+//     props: {
+//       product
+//     }
+//   }
+// }
